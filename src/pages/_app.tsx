aee62a311eb56e871a77b83b1282b480403d5d72
Joao <jp_bast@hotmail.com>
2021-04-23 21:05:15 -0300

    updating inital files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import styles from '../styles/app.module.scss'
+import Header from '../components/Header'
+import Player from '../components/Player'
+import { PlayProvider } from '../contexts/PlayContext'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    
+    <div className={styles.wrapper}>
+      <Head>
+        <title>Podcastr</title>
+      </Head>
+      <PlayProvider>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </PlayProvider>
+    </div>
+  )
 }
 
 export default MyApp
